fa824a78922b36a452d303c27bb687aaf81c056b
92.6 <siefeldinessam02@gamil.com>
2025-08-28 22:04:35 +0300

    fix directory

--- course-management/src/main/java/com/myprojects/course_management/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myprojects.course_management.repository;
-
-import com.myprojects.course_management.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
--- course-management/src/main/java/com/myprojects/course_management/repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myprojects.course_management.repository;
-
-import com.myprojects.course_management.entity.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
